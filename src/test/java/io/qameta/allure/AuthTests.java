67310953d8a2063c06dd68c2b089a32f53bd04e7
Kirill Katkov <k.katkov@dodopizza.com>
2022-06-28 11:21:41 +0300

    Update

--- src/test/java/io/qameta/allure/AuthTests.java
+++ src/test/java/io/qameta/allure/AuthTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import static io.qameta.allure.Allure.step;
 
 public class AuthTests {
+
     @Test
     @AllureId("10721")
     @DisplayName("Авторизация через Google")

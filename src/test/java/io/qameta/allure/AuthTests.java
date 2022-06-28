63394aead4a8d3090a3ada2c2f36a4467320ba1f
Kirill Katkov <k.katkov@dodopizza.com>
2022-06-28 12:02:20 +0300

    Update

--- src/test/java/io/qameta/allure/AuthTests.java
+++ src/test/java/io/qameta/allure/AuthTests.java
@@ -33,4 +33,53 @@ public class AuthTests {
         });
         step("Разлогиниваемся");
     }
+
+
+
+    @Test
+    @AllureId("10728")
+    @DisplayName("Авторизация через GitHub")
+    @Tags({@Tag("blocker"), @Tag("web")})
+    public void testGitHubAuth() {
+        step("Открываем главную страницу");
+        step("Нажимаем кнопку Авторизация");
+        step("Выбираем способ авторизации через GitHub");
+        step("Авторизуемся как пользователь user_name", () -> {
+            step("Вводим логин user_login");
+            step("Вводим пароль user_password");
+            step("Нажимаем кнопку Войти");
+        });
+        step("Должны оказаться на главной странице сайта");
+        step("Профиль пользователя должен быть заполнен из GitHub", () -> {
+            step("Имя user_name");
+            step("Login user_login");
+        });
+        step("Разлогиниваемся");
+    }
+
+
+    @Test
+    @AllureId("10727")
+    @DisplayName("Авторизация через Facebook")
+    @Tags({@Tag("blocker"), @Tag("web")})
+    public void testFacebookAuth() {
+        step("Открываем главную страницу");
+        step("Нажимаем кнопку Авторизация");
+        step("Выбираем способ авторизации через Facebook");
+        step("Авторизуемся как пользователь Artem Eroshenko", () -> {
+            step("Вводим логин eroshenkoam@gmail.com");
+            step("Вводим пароль 12398123981231");
+            step("Нажимаем кнопку Войти");
+        });
+        step("Должны оказаться на главной странице сайта");
+        step("Профиль пользователя должен быть заполнен из GoogleFacebook", () -> {
+            step("Имя Artem Eroshenko");
+            step("Login eroshenkoam");
+            step("Авататарка");
+        });
+        step("Разлогиниваемся");
+    }
+
+
+
 }
